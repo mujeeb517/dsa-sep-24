ac1662e7a0b70b4555fb3171f0b2977cc19648eb
Mujib <mujib@Mujibs-MacBook-Pro-2.local>
2024-09-22 08:32:20 +0530

    initial commit

---
+++ index.py
@@ -0,0 +1,24 @@
+# output statement
+print("hello world")
+
+# Data types 
+# variable (memory location), temporary RAM
+# size
+# declaration, initialization
+# lower case, snake casing
+# Red bus
+# serialization
+# deserialization
+# conditional, loops, classes, objects
+age = 10
+
+# 32 bit
+# 0 010101010101010101010
+# 2^10 = 1024 = 10^3
+# 2^10 . 2^ 10. 2^10. 2^2 = 
+# 10^3. 10^3. 10^3  = 10^9
+
+# constraints
+# -10^9 <= i <= 10^9 
+# -10^18 <= i <= 10^18 
+
