74335c91052d2d9f274a03eb6b816c09044c85a3
Mujib <mujib@Mujibs-MacBook-Pro-2.local>
2024-10-06 10:29:37 +0530

    binary search

--- arrays/problems.py
+++ arrays/problems.py
@@ -15,23 +15,82 @@
 # [1,1,1,2,2,4,5,5,5,5,5,6,7,8,9,10]
 def lin_search(arr, k):
     for elem in arr:
-        if(elem==k):
+        if (elem == k):
             return True
-    
+
     return False
 
+
 def bin_search(arr, k):
-    left=0
+    left = 0
     right = len(arr)-1
 
-    while(left <= right):
+    while (left <= right):
         mid = (left+right) // 2
-        if(arr[mid] == k):
+        if (arr[mid] == k):
             return True
-        elif(arr[mid] > k):
+        elif (arr[mid] > k):
             right = mid-1
         else:
             left = mid+1
-    
+
     return False
 
+# O(LogN), O(1)
+
+
+def sqrt(n):
+    low, high = 1, n
+    while (low <= high):
+        # mid = (low+high) // 2
+        mid = low + ((high-low)//2)
+        val = mid * mid
+        if (val == n):
+            return mid
+        if (val > n):
+            high = mid-1
+        else:
+            low = mid+1
+    return -1
+
+
+def floor(arr, k):
+    ans = -1
+    low, high = 0, len(arr)-1
+    while (low <= high):
+        mid = low + ((high-low)//2)
+        if (arr[mid] == k):
+            ans = mid
+            high = mid-1
+        elif (arr[mid] > k):
+            high = mid-1
+        else:
+            low = mid+1
+
+    return ans
+
+
+def ceil(arr, k):
+    ans, low, high = -1, 0, len(arr)-1
+
+    while (low <= high):
+        mid = low + ((high-low)//2)
+        if (arr[mid] == k):
+            ans = mid
+            low = mid+1
+        elif (arr[mid] > k):
+            high = mid-1
+        else:
+            low = mid+1
+
+    return ans
+
+
+def freq(arr, k):
+    start_index = floor(arr, k)
+    end_index = ceil(arr, k)
+
+    return 0 if start_index == -1 else end_index - start_index + 1
+
+# given a rotated sorted array find the min number
+# [4 5 6 1 2 3]
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from general.problems import count_factors_optimized, is_prime,gcd,lcm
-from arrays.problems import lin_search, bin_search
+from general.problems import count_factors_optimized, is_prime, gcd, lcm
+from arrays.problems import lin_search, bin_search, sqrt, floor, ceil, freq
 
 # 1,2,3,4,6,12
 # [10,20,30,40,50,60,70] k: 50
+
+
 def main():
-  arr = [ 10,20,30,40,50,60,70]
-  res = bin_search(arr, 55)
-  print(res)
+    print(freq([1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 6], 2))
+    print(freq([1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 6], 3))
+    print(freq([1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 6], 4))
+    print(freq([1, 1, 2, 2, 2, 2, 3, 3, 4, 5, 6], 8))
 
 
 if __name__ == '__main__':
