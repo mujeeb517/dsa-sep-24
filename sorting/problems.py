beb03a2bd58f0e6980c9c44a23b9c9690e3db213
Mujib <mujib@Mujibs-MacBook-Pro-2.local>
2024-10-26 11:11:28 +0530

    fix 012 segregation algorithm

--- sorting/problems.py
+++ sorting/problems.py
@@ -13,16 +13,18 @@ def segregate_01(arr):
 
     return arr
 
+
 def segregate_012(arr):
     n = len(arr)
-    p0, p1, p2 = 0, 1, n-1
+    p0, p1, p2 = 0, 0, n-1
 
-    while (p1 <= p2 and p0 <= p2):
+    while (p1 <= p2):
         if arr[p1] == 1:
             p1 += 1
         elif arr[p1] == 0:
             arr[p0], arr[p1] = arr[p1], arr[p0]
             p0 += 1
+            p1 += 1
         else:
             arr[p2], arr[p1] = arr[p1], arr[p2]
             p2 -= 1
